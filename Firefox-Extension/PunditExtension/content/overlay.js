63a31401495c4c91276ac3b3f9c2ee6be16672dd
Michele Nucci <mik.nucci@gmail.com>
2013-05-03 13:06:42 +0200

    Initial tests

--- Firefox-Extension/PunditExtension/content/overlay.js
+++ Firefox-Extension/PunditExtension/content/overlay.js
@@ -1,12 +1,12 @@
-var HelloWorld = {
+var Pundit = {
   onLoad: function() {
     // initialization code
     this.initialized = true;
   },
 
   onMenuItemCommand: function() {
-    window.open("chrome://helloworld/content/hello.xul", "", "chrome");
+    window.open("chrome://PunditExtension/content/pundit.xul", "", "chrome");
   }
 };
 
-window.addEventListener("load", function(e) { HelloWorld.onLoad(e); }, false); 
+window.addEventListener("load", function(e) { Pundit.onLoad(e); }, false); 
